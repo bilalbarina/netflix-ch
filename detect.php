9a6dd46adbfd004b48ebcd2ca02ab95042f6e9a2
bilalbarina <bilalbarina1@gmail.com>
2020-08-13 14:42:14 +0100

    Update detect.php

--- detect.php
+++ detect.php
@@ -2,7 +2,7 @@
 
 $ip = $_SERVER['REMOTE_ADDR'];
 $url = "http://ip-api.com/json/" . $ip . "?fields=asname";
-$getJson = file_get_contents($url);
+$getJson = file_get_contents("$url");
 $json = json_decode($getJson, true);
 
 $asname = $json['asname'];
